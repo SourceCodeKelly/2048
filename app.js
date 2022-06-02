f290ca2e3ea07abba3c5e8e5ce0dc1dba4bf9458
Anthony <96215255+SourceCodeKelly@users.noreply.github.com>
2022-06-02 17:13:11 -0400

    Arrow key functionality set

--- app.js
+++ app.js
@@ -51,3 +51,24 @@ function updateTile(tile, num) {
     }
 }
 
+document.addEventListener('keyup', (e) => {
+    if (e.code == "ArrowLeft") {
+        slideLeft();
+        setTwo();
+    }
+    else if (e.code == "ArrowRight") {
+        slideRight();
+        setTwo();
+    }
+    else if (e.code == "ArrowUp") {
+        slideUp();
+        setTwo();
+
+    }
+    else if (e.code == "ArrowDown") {
+        slideDown();
+        setTwo();
+    }
+    document.getElementById("score").innerText = score;
+})
+
